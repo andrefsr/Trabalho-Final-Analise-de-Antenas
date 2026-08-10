9309f2fbde25d877ceeea36d1a23a99080b85731
André Fabiano <andrefsr1902@gmail.com>
2026-08-10 13:49:21 -0300

    Regra de Simpson validada.

---
+++ Parte 1/integrador.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+### Intervalo de análise
+x2 = 1
+x1 = 0
+
+n = 102  ### Número de pontos
+
+def f(x):
+  return x**2 
+
+### Simpson
+
+def SimpsonInt(a,b,num):
+
+    if num % 2 == 0:
+        num += 1
+
+    pontos = np.linspace(a,b,num)
+
+    h = (b-a)/(num-1)
+
+    I = f(a) + f(b)
+    n = len(pontos)
+    for i in range(1,num-1):
+
+        if i % 2 == 0: I += 2*f(pontos[i])
+        else: I += 4*f(pontos[i])
+
+    I = (h/3) * I
+    return I
+
+simpson = SimpsonInt(x1,x2,n)
+
+
+
+### Trapézios repetidos
+
+# x = np.linspace(0, 1, 1000000)
+
+# y = f(x)
+
+# area = np.trapezoid(y, x)
+
+# gabarito = 1/3
+
+# erro_relativo_trapezio = np.abs(gabarito-trapezio)/gabarito
+# erro_relativo_simpson = np.abs(gabarito-simpson)/gabarito
+# erro_relativo_numpy = np.abs(gabarito-area)/gabarito
+
+#print(f'Erro relativo trapézio = {erro_relativo_trapezio}\n')
+#print(f'Erro relativo simpson = {erro_relativo_simpson}\n')
+#print(f'Erro relativo numpy = {erro_relativo_numpy}')
+
+#def intnumtr(a,b,num):
+#    pontos = np.linspace(a,b,num)
+
+#    h = (b-a)/(num-1)
+
+#    I = f(a) + f(b)
+#    for i in range(1,num-1):
+#        I += 2*f(pontos[i])
+
+#    I = (h/2)*I
+#    return I
+
+#trapezio = intnumtr(x1,x2,n)
